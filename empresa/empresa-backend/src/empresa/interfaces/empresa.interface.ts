e8894bb7132abc9ee40349e50877937deb153000
ShinjiYo <61997709+ShinjiYo@users.noreply.github.com>
2021-07-04 22:44:35 -0300

    Update empresa.interface.ts

--- empresa/empresa-backend/src/empresa/interfaces/empresa.interface.ts
+++ empresa/empresa-backend/src/empresa/interfaces/empresa.interface.ts
@@ -1,7 +1,7 @@
 import { Document } from 'mongoose';
 
 export interface Empresa extends Document {
-    readonly first_name: string;
+    readonly nome: string;
     readonly telefone: string;
     readonly estaAtiva: string;
 }
